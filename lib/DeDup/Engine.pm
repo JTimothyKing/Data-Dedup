e53c49d2eacd8d776bcbb609a4c3c1182d4befdf
Tim King <timk@JTimothyKing.com>
2013-12-30 16:06:02 -0500

    more refactorings

--- lib/DeDup/Engine.pm
+++ lib/DeDup/Engine.pm
@@ -1,8 +1,10 @@
 use strict;
 use warnings;
 use mop;
+use signatures;
 
-use Scalar::Util 'blessed';
+use Scalar::Util ();
+use List::MoreUtils ();
 
 =head1 NAME
 
@@ -38,6 +40,16 @@ DeDup::Engine - A general-purpose deduplication engine
 
 class DeDup::Engine {
     has $!blocking is ro; # a sub or array of subs
+
+    sub _is_code($r) { eval { ref $r && \&{$r} } }
+    sub _is_array($r) { eval { ref $r && \@{$r} } }
+
+    method BUILD {
+        $!blocking = sub { $_[0] } unless defined $!blocking;
+        $!blocking = [ $!blocking ] if _is_code($!blocking);
+        die "blocking attribute must be a coderef or an arrayref of coderefs"
+            unless _is_array($!blocking) && List::MoreUtils::all { _is_code($_) } @{$!blocking};
+    }
 
 
     class DeDup::Engine::Block is repr('HASH') {
@@ -75,54 +87,61 @@ class DeDup::Engine {
     has $!_blocks_by_key; # may contain a BlockKeyStore or Block
 
 
+    method _block($object, $blocking_sub, $r_keystore, $keys) {
+        $keys //= [];
+
+        my $keystore_isa = sub ($class) {
+            Scalar::Util::blessed($$r_keystore) && $$r_keystore->isa($class)
+        };
+
+
+        if ($keystore_isa->('DeDup::Engine::Block')) {
+            # Key this previous block without key
+            my $block = $$r_keystore;
+
+            my $key = $blocking_sub->(
+                $block->object(0), # blocks without keys can only have one object
+            );
+
+            $block->add_keys($key); # no longer without a key
+
+            $$r_keystore = DeDup::Engine::BlockKeyStore->new;
+            $$r_keystore->set($key => $block);
+        }
+
+
+        my $r_block; # the block slot into which to place the object
+
+        if ($keystore_isa->('DeDup::Engine::BlockKeyStore')) {
+            # Find an existing block, if one exists
+            my $key = $blocking_sub->($object);
+            push @$keys, $key;
+            $r_block = $$r_keystore->get_ref($key); # creates a new slot if needed
+
+        } else {
+            # Still must add the very first block
+            $r_block = $r_keystore;
+        }
+
+
+        if (! $$r_block) {
+            $$r_block = DeDup::Engine::Block->new(
+                keys => [@$keys],
+                objects => [],
+            );
+            push @{$!_blocks}, $$r_block;
+        }
+
+        $$r_block->add_objects($object);
+    }
+
     method add(@objects) {
         for my $object (@objects) {
-            my @keys;
-
-            my $blocking_sub = ref $!blocking eq 'CODE' ? $!blocking
-                : ref $!blocking eq 'ARRAY' ? $!blocking->[0]
-                : die("Invalid blocking sub");
+            my $blocking_sub = $!blocking->[0];
 
             my $r_keystore = \($!_blocks_by_key);
 
-            if (blessed($$r_keystore) && $$r_keystore->isa('DeDup::Engine::Block')) {
-                # Key this previous block without key
-                my $block = $$r_keystore;
-
-                my $key = $blocking_sub->(
-                    $block->object(0), # blocks without keys can only have one object
-                );
-
-                $block->add_keys($key); # no longer without a key
-
-                $$r_keystore = DeDup::Engine::BlockKeyStore->new;
-                $$r_keystore->set($key => $block);
-            }
-
-
-            my $r_block;
-
-            if (blessed($$r_keystore) && $$r_keystore->isa('DeDup::Engine::BlockKeyStore')) {
-                # Find an existing block, if one exists
-                my $key = $blocking_sub->($object);
-                push @keys, $key;
-                $r_block = $$r_keystore->get_ref($key); # creates a new slot if needed
-
-            } else {
-                # Still must add the very first block
-                $r_block = $r_keystore;
-            }
-
-
-            if (! $$r_block) {
-                $$r_block = DeDup::Engine::Block->new(
-                    keys => [@keys],
-                    objects => [],
-                );
-                push @{$!_blocks}, $$r_block;
-            }
-
-            $$r_block->add_objects($object);
+            $self->_block($object, $blocking_sub, $r_keystore);
         }
     }
 
