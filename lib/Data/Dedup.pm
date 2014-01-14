d1d038d0b3c67dd80ed1553c8ac9fb5717c84c58
Tim King <timk@JTimothyKing.com>
2014-01-14 15:03:18 -0500

    some links in Data::Dedup

--- lib/Data/Dedup.pm
+++ lib/Data/Dedup.pm
@@ -9,6 +9,14 @@ use warnings;
 
 Data::Dedup - A general-purpose deduplication framework
 
+=head1 SEE ALSO
+
+L<Data::Dedup::Engine> - the core deduplication engine
+
+L<Data::Dedup::Files> - a file deduplicator
+
+L<dedup_files> - command-line file deduplicator app
+
 =cut
 
 1;
